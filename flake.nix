532bd9a51329cef8aff7db99e7d1485b69baebc4
Simon-Weij <simongvanderweij@gmail.com>
2025-08-09 18:35:41 +0200

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forEachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.python3Packages.buildPythonPackage {
+            pname = "apm";
+            version = "0.1.0";
+            src = ./.;
+            format = "setuptools";
+
+            propagatedBuildInputs = [
+            ];
+
+          };
+        });
+
+      devShells = forEachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python3
+              pkgs.python3Packages.pip
+              pkgs.python3Packages.setuptools
+            ];
+            shellHook = ''
+              pip install --e .
+            '';
+          };
+        });
+    };
+}
