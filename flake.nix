fbf5092495a61a3f19d1736b6e3033d3abe5459e
Simon-Weij <simongvanderweij@gmail.com>
2025-08-22 12:01:11 +0200

    added nix-index database and did some cleaning up

--- flake.nix
+++ flake.nix
@@ -1,40 +1,35 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      packages = forEachSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nix-index-database, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages = {
           default = pkgs.python3Packages.buildPythonPackage {
             pname = "apm";
             version = "0.1.0";
             src = ./.;
             format = "setuptools";
           };
-        });
-
-      devShells = forEachSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
+        };
+        devShells = {
           default = pkgs.mkShell {
             packages = [
               pkgs.python3
               pkgs.python3Packages.pip
               pkgs.python3Packages.setuptools
+              nix-index-database.packages.${system}.default
             ];
-            shellHook = ''
-              pip install --e .
-            '';
           };
-        });
-    };
+        };
+      }
+    );
 }
